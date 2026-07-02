436ad6cbde7fcc1867027acf1bf0680e35d2887c
Oyatulloh <sizning_emailingiz@gmail.com>
2026-07-02 21:02:41 +0500

    Added GET and POST endpoints for Products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() data: { name: string; price: number }) {
+    return this.productsService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: { name: string; price: number }) {
+    return this.prisma.product.create({
+      data,
+    });
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+}
